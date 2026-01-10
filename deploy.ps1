1066dd23eb2b48829b70f60701f89c9a3c9bf188
phillongworth <PHIL.LONGWORTH@GMAIL.COM>
2026-01-10 21:14:06 +0000

    revised deploy

--- deploy.ps1
+++ deploy.ps1
@@ -1,2 +1,7 @@
+# 1. Define your variables clearly
+$SERVER = "myserver"  # Replace with your actual server IP
 $REMOTE_PATH = "/var/www/phillongworth-co-uk/html"
+
+# 2. Run the SCP command
+# Note the double quotes and the variable placement
 scp -r ./* "${SERVER}:${REMOTE_PATH}"
